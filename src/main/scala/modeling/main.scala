e9882fbc50a9b7cbfb42182d5db3908d25259650
Andrew Brooks <andrewbrooksct@gmail.com>
2016-03-25 16:31:26 -0400

    adding in Option parameter to alignedData class

--- src/main/scala/modeling/main.scala
+++ src/main/scala/modeling/main.scala
@@ -15,33 +15,41 @@ import training.cnn._
 object runPipeline {
   def main(args: Array[String]): Unit = {
     
-  // image processing
-  val labMap = readBizLabels("/Users/abrooks/Box Sync/Andrew Brooks/kaggle_yelp/data/labels/train.csv")
-  val bizMap = readBiz2ImgLabels("/Users/abrooks/Documents/kaggle_yelp_photo/train_photo_to_biz_ids.csv")
-  val imgs = getImageIds("/Users/abrooks/Documents/kaggle_yelp_photo/train_photos/", bizMap, bizMap.map(_._2).toSet.slice(0,1000).toList).slice(0,10)
+  // image processing on training data
+  val labMap = readBizLabels("data/labels/train.csv")
+  val bizMap = readBiz2ImgLabels("data/labels/train_photo_to_biz_ids.csv")
+  val imgs = getImageIds("data/images/train", bizMap, bizMap.map(_._2).toSet.slice(0,1000).toList).slice(0,10)
   val dataMap = processImages(imgs, resizeImgDim = 128) // nPixels = 64
-  val alignedData = new alignedData(dataMap, bizMap, labMap)()
+  val alignedData = new alignedData(dataMap, bizMap, Option(labMap))()
   
   // training (one model/class)
   println("processing DONE")
-  val cnn = trainModel(alignedData, bizClass = 2, saveNN = "results/modelsV1/model2_iter3_img1500_trash") // many microparameters hardcoded within 
+  val cnn0 = trainModel(alignedData, bizClass = 0, saveNN = "results/modelsV0/model0") // many microparameters hardcoded within
+  val cnn1 = trainModel(alignedData, bizClass = 1, saveNN = "results/modelsV0/model1") // many microparameters hardcoded within
+  val cnn2 = trainModel(alignedData, bizClass = 2, saveNN = "results/modelsV0/model2") // many microparameters hardcoded within
+  val cnn3 = trainModel(alignedData, bizClass = 3, saveNN = "results/modelsV0/model3") // many microparameters hardcoded within
+  val cnn4 = trainModel(alignedData, bizClass = 4, saveNN = "results/modelsV0/model4") // many microparameters hardcoded within
+  val cnn5 = trainModel(alignedData, bizClass = 5, saveNN = "results/modelsV0/model5") // many microparameters hardcoded within
+  val cnn6 = trainModel(alignedData, bizClass = 6, saveNN = "results/modelsV0/model6") // many microparameters hardcoded within
+  val cnn7 = trainModel(alignedData, bizClass = 7, saveNN = "results/modelsV0/model7") // many microparameters hardcoded within
+  val cnn8 = trainModel(alignedData, bizClass = 8, saveNN = "results/modelsV0/model8") // many microparameters hardcoded within
+
+  // processing test data for scoring
+  val bizMapTE = readBiz2ImgLabels("data/labels/test_photo_to_biz.csv")
+  val imgsTE = getImageIds("data/images/test/", bizMapTE, bizMapTE.map(_._2).toSet.slice(0,12).toList).slice(0, 100)
+  val dataMapTE = processImages(imgsTE, resizeImgDim = 128)
+  val alignedDataTE = new alignedData(dataMapTE, bizMapTE, None)()
   
-//  // scoring (one model/class)
-  val bizMapTE = readBiz2ImgLabels("/Users/abrooks/Documents/kaggle_yelp_photo/test_photo_to_biz.csv")
-  val imgsTE = getImageIds("/Users/abrooks/Documents/kaggle_yelp_photo/test_photos/", bizMapTE, bizMapTE.map(_._2).toSet.slice(0,12).toList)
-  val dataMapTE = processImages(imgsTE, resizeImgDim = 128, nPixels = 64)
-  val alignedDataTE = new alignedData(dataMapTE, bizMapTE)(List(2,3))
+  // creating csv file to submit to kaggle (scores all models)
+  val kaggleResults = createKaggleSubmitObj(alignedDataTE, "results/ModelsV0/")
+  val kaggleSubmitResults = writeKaggleSubmissionFile("results/kaggleSubmission/kaggleSubmit_3_24_2016_trash.csv", kaggleResults, thresh = 0.5)
+  
+  // example of how to score just model
   val dsTE = makeDataSetTE(alignedDataTE)
-  val model = loadNN("/Users/abrooks/Box Sync/Andrew Brooks/kaggle_yelp/data/results/coefficients1.json", "/Users/abrooks/Box Sync/Andrew Brooks/kaggle_yelp/data/results/coefficients1.bin")
+  val model = loadNN("results/modelsV0/model1.json", "results/modelsV0/model1.bin")
   val predsTE = scoreModel(model, dsTE)
   val bizScoreAgg = aggImgScores2Biz(predsTE, alignedDataTE)
-  
-  // creating csv file to submit to kaggle
-  val kaggleResults = createKaggleSubmitObj(alignedDataTE, "/Users/abrooks/Box Sync/Andrew Brooks/kaggle_yelp/results/")
-  val kaggleSubmitResults = writeKaggleSubmissionFile("results/kaggleSubmission/kaggleSubmit.csv", kaggleResults, thresh = 0.5)
-  
-   println(kaggleSubmitResults)
-   println(kaggleResults)
+  println(bizScoreAgg)
   
   }
 }
--- src/main/scala/modeling/processing/alignedData.scala
+++ src/main/scala/modeling/processing/alignedData.scala
@@ -2,11 +2,11 @@ package modeling.processing
 
 //import modeling.processing.aligningData.alignBizImgIds
 
-class alignedData(dataMap: Map[Int, Vector[Int]], bizMap: Map[Int, String], labMap: Map[String, Set[Int]])
+class alignedData(dataMap: Map[Int, Vector[Int]], bizMap: Map[Int, String], labMap: Option[Map[String, Set[Int]]])
                  (rowindices: List[Int] = dataMap.keySet.toList) {
   
   // initializing alignedData with empty labMap when it is not provided (we are working with training data)
-  def this(dataMap: Map[Int, Vector[Int]], bizMap: Map[Int, String])(rowindices: List[Int]) = this(dataMap, bizMap, Map("" -> Set[Int]()))(rowindices)
+  def this(dataMap: Map[Int, Vector[Int]], bizMap: Map[Int, String])(rowindices: List[Int]) = this(dataMap, bizMap, None)(rowindices)
 
   def alignBizImgIds(dataMap: Map[Int, Vector[Int]], bizMap: Map[Int, String])
     (rowindices: List[Int] = dataMap.keySet.toList): List[(Int, String, Vector[Int])] = {
@@ -19,14 +19,16 @@ class alignedData(dataMap: Map[Int, Vector[Int]], bizMap: Map[Int, String], labM
       }
   }
   
-  def alignLabels(dataMap: Map[Int, Vector[Int]], bizMap: Map[Int, String], labMap: Map[String, Set[Int]])
+  def alignLabels(dataMap: Map[Int, Vector[Int]], bizMap: Map[Int, String], labMap: Option[Map[String, Set[Int]]])
     (rowindices: List[Int] = dataMap.keySet.toList): List[(Int, String, Vector[Int], Set[Int])] = {
       def flatten1[A, B, C, D](t: ((A, B, C), D)): (A, B, C, D) = (t._1._1, t._1._2, t._1._3, t._2)
-      val al = alignBizImgIds(dataMap, bizMap)()
+      val al = alignBizImgIds(dataMap, bizMap)(rowindices)
       for { p <- al
       } yield {
         val bid = p._2
-        val labs = if (labMap.keySet.contains(bid)) labMap(bid) else Set[Int]() 
+        val labs = if(labMap == None) Set[Int]() else { 
+          if (labMap.get.keySet.contains(bid)) labMap.get(bid) else Set[Int]()
+        }
         flatten1(p, labs) 
       }
   }
@@ -39,6 +41,7 @@ class alignedData(dataMap: Map[Int, Vector[Int]], bizMap: Map[Int, String], labM
   def getBizIds = data.map(_._2)
   def getImgVectors = data.map(_._3)
   def getBizLabels = data.map(_._4)
+  
   
   
   
